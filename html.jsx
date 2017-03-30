d2314f7320d09595e95b9d2d9dcc03460bb4d8b3
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-03-30 14:30:18 -0600

    server-side render css

--- html.jsx
+++ html.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { prefixLink } from 'gatsby-helpers';
+import styleSheet from 'styled-components/lib/models/StyleSheet';
 
 const BUILD_TIME = new Date().getTime()
 
@@ -13,6 +14,12 @@ module.exports = React.createClass({
   render () {
     const head = Helmet.rewind();
 
+    let css;
+    if (process.env.NODE_ENV === 'production') {
+      const styles = styleSheet.rules().map(rule => rule.cssText).join('\n');
+      css = <style dangerouslySetInnerHTML={{ __html: styles }} />;
+    }
+
     return (
       <html lang="en">
         <head>
@@ -21,6 +28,7 @@ module.exports = React.createClass({
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           {head.title.toComponent()}
           {head.meta.toComponent()}
+          {css}
         </head>
         <body>
           <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
