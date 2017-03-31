d65d02b25ad58da939d59dff70dfa0058dd97290
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-03-30 19:08:15 -0600

    sample components

--- components/styled/index.js
+++ components/styled/index.js
@@ -1,5 +1,6 @@
 import { injectGlobal } from 'styled-components';
 import styled from 'styled-components';
+import { darken } from 'polished';
 import { Link } from 'react-router';
 
 /*
@@ -29,7 +30,7 @@ export const Wrapper = styled.div`
    font-size: 30px;
  `;
 
- /*
+/*
  * Subtitle
  */
 export const Subtitle = styled.h2`
@@ -53,3 +54,106 @@ export const StyledLink = styled(Link)`
    color: palevioletred;
    font-size: 18px;
  `;
+
+/*
+ * Message
+ */
+export const Message = styled.h2`
+  font-size: 27px;
+`;
+
+/*
+ * Button
+ */
+export const Button = styled.button`
+  padding: ${props => props.large ? '16px 48px' : '8px 24px'};
+  color: white;
+  font-size: 16px;
+  font-weight: 600;
+  background-color: black;
+  border-radius: 4px;
+  border: 0;
+  cursor: pointer;
+`;
+
+/*
+ * DefaultButton
+ */
+export const DefaultButton = styled(Button)`
+  background-color: lightblue;
+  &:hover {
+    background: ${darken(0.1, '#add8e6')};
+  }
+`;
+
+/*
+ * PrimaryButton
+ */
+export const PrimaryButton = styled(Button)`
+  background-color: lightseagreen;
+  &:hover {
+    background: ${darken(0.1, '#20b2aa')};
+  }
+`;
+
+/*
+ * Success
+ */
+export const SuccessButton = styled(Button)`
+  background-color: lightgreen;
+  &:hover {
+    background: ${darken(0.1, '#90ee90')};
+  }
+`;
+
+/*
+ * Warning
+ */
+export const WarningButton = styled(Button)`
+  background-color: lightsalmon;
+  &:hover {
+    background: ${darken(0.1, '#ffa07a')};
+  }
+`;
+
+/*
+ * Danger
+ */
+export const DangerButton = styled(Button)`
+  background-color: lightcoral;
+  &:hover {
+    background: ${darken(0.1, '#f08080')};
+  }
+`;
+
+
+/*
+ * Input
+ */
+export const Input = styled.input`
+  color: ${props => props.color};
+  font-size: 16px;
+  padding: ${props => props.large ? '16px 48px' : '8px 24px'};
+  border: 2px solid ${props => props.color};
+  background: transparent;
+`;
+
+/*
+ * ComponentsWrapper
+ */
+export const ComponentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 5%;
+`;
+
+/*
+ * ComponentsWrapperInner
+ */
+export const ComponentsWrapperInner = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
+  margin-bottom: 35px;
+`;
--- pages/components.jsx
+++ pages/components.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { config } from 'config';
 import _ from 'lodash';
+import {
+  ComponentsWrapper,
+  ComponentsWrapperInner,
+  Message,
+  Subtitle,
+  DefaultButton,
+  PrimaryButton,
+  SuccessButton,
+  WarningButton,
+  DangerButton,
+  Input
+} from '../components/styled/';
 
 export default class Components extends React.Component {
   render() {
@@ -9,10 +21,38 @@ export default class Components extends React.Component {
     const post = route.page.file;
 
     return (
-      <div>
+      <ComponentsWrapper>
         <Helmet title={`${config.siteTitle} - ${_.capitalize(post.name)}`} />
-        <h3>Wow! Components!</h3>
-      </div>
+        <Message>Meet some superstylin' components</Message>
+
+        <Subtitle>Buttons</Subtitle>
+        <ComponentsWrapperInner>
+          <DefaultButton>Default</DefaultButton>
+          <PrimaryButton>Primary</PrimaryButton>
+          <SuccessButton>Success</SuccessButton>
+          <WarningButton>Warning</WarningButton>
+          <DangerButton>Danger</DangerButton>
+        </ComponentsWrapperInner>
+
+        <Subtitle>Inputs</Subtitle>
+        <ComponentsWrapperInner>
+          <Input color="red" placeholder="Red" />
+          <Input color="rebeccapurple" placeholder="Rebeccapurple" />
+          <Input color="salmon" placeholder="Salmon" />
+          <Input color="goldenrod" placeholder="Goldenrod" />
+        </ComponentsWrapperInner>
+
+        <Subtitle>Variants</Subtitle>
+        <ComponentsWrapperInner>
+          <PrimaryButton large>Primary</PrimaryButton>
+          <SuccessButton large>Success</SuccessButton>
+          <DangerButton large>Danger</DangerButton>
+        </ComponentsWrapperInner>
+        <ComponentsWrapperInner>
+          <Input large color="rebeccapurple" placeholder="Rebeccapurple" />
+          <Input large color="goldenrod" placeholder="Goldenrod" />
+        </ComponentsWrapperInner>
+      </ComponentsWrapper>
     );
   }
 }
